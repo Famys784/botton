9b32cd8f7f99cd73f1f21611a34fddc222e4db6a
Famys 784 <Famys784@outlook.com>
2023-06-24 11:41:59 +0100

    first commit

---
+++ fon.js
@@ -0,0 +1,12 @@
+function myFunction(){
+    var name;
+    name = "Toni"
+    var schoolclass;
+   schoolclass = "Jss2"
+   var bestfriend;
+   bestfriend = "Feranmi"
+    
+    document.getElementById("demo").innerHTML =name ;
+    document.getElementById("name").innerHTML = schoolclass ;
+    document.getElementById("friend").innerHTML =bestfriend
+}
